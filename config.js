769c218d945bb2b35b2ec0b7c4c840c96b383a5c
thiscold829 <107271781+thiscold829@users.noreply.github.com>
2026-04-29 21:18:47 +0900

    Update API_URL in config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // config.js
 const CONFIG = {
   SHEET_ID: '16izs14p3VvDfojdBR2Q3Qwty6wHZiRqNbnQ8PxS9eOY',
-  API_URL: 'https://script.google.com/macros/s/AKfycbxwcCJ9LT6aDX8ZPRvSY8pK2_vMyPg1EPNRcyF77h_cpMuCIU8wUiZk8JujLbfeBJgq/exec'
+  API_URL: 'https://script.google.com/macros/s/AKfycbzlliwfDq5C5f_KtcBDqJr7Am1Jf8Vh41RozM6FCcFgHsJMkzyryvwOsEHfYjWF2yAX/exec'
 };
