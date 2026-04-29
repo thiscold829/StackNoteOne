c7944d922585fa63a00695cac436cf11c3f544b6
thiscold829 <107271781+thiscold829@users.noreply.github.com>
2026-04-29 20:25:34 +0900

    시트api 통일화 기능 추가

---
+++ config.js
@@ -0,0 +1,5 @@
+// config.js
+const CONFIG = {
+  SHEET_ID: '16izs14p3VvDfojdBR2Q3Qwty6wHZiRqNbnQ8PxS9eOY',
+  API_URL: 'https://script.google.com/macros/s/AKfycbxwcCJ9LT6aDX8ZPRvSY8pK2_vMyPg1EPNRcyF77h_cpMuCIU8wUiZk8JujLbfeBJgq/exec'
+};
